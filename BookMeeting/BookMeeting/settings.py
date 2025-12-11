84f33d444c82581f1d0d5fdf7a03c570a3e3c658
namlh <nam.lehuy@digitalmedia.com.vn>
2025-12-11 11:20:51 +0700

    remove env

--- BookMeeting/BookMeeting/settings.py
+++ BookMeeting/BookMeeting/settings.py
@@ -225,5 +225,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'nguyennguyen19052000@gmail.com'
-EMAIL_HOST_PASSWORD = 'yoxajnwrjnfokcdb'
+EMAIL_HOST_USER = 'admin'
+EMAIL_HOST_PASSWORD = 'admin'
